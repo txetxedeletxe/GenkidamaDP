58dc6e49806dafe177a0a2ff973b0a2d9a1e37d1
sophyelord <sophyelord@Ariel-III>
2019-03-22 19:59:39 +0100

    Rebaseline commit

--- GenkidamaDP/src/data/GenkidamaData.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-import java.io.Serializable;
-
-public interface GenkidamaData extends Serializable{
-
-}
--- GenkidamaDP/src/interfaces/Factory.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface Factory<T> {
-
-	public T construct();
-	
-	
-}
