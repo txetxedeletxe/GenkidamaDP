afc5e79ad7a500eccfddd824cf945a0a3a92804d
sophyelord <sophyelord@Ariel-IV>
2019-08-05 00:12:08 +0200

    Slight changes definition

--- src/command/ACKCommand.java
+++ src/command/ACKCommand.java
@@ -1,13 +1,24 @@
 package command;
 
+import command.ProtoCommand.ProtoType;
+
 public class ACKCommand extends MetaCommand{
 
+	// Command info
 	private short commandId;
+	private ProtoType commandType;
 	
-	public ACKCommand() {
+	// result code
+	private byte resutlCode;
+	
+	public ACKCommand(short commandId, ProtoType commandType, byte resultCode) {
 		super(MetaType.ACK);
 		
-		
+		this.commandId = commandId;
+		this.commandType = commandType;
+		this.resutlCode = resultCode;
 	}
+	
+	
 
 }
